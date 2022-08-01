e413eaeec4a33e21302b97a9773a1c2a3d51c5ef
BrayanAmaya <brayanantonio816@gmail.com>
2022-08-01 11:08:53 -0600

    update

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <vue-app />
+</template>
+ 
+
+<script>
+import VueApp from "./components/VueApp.vue";
+
+export default {
+
+  components: { VueApp },
+  name: "App",
+
+  mounted() {
+    axios({
+      method: 'post',
+      url: 'http://api_airbnb.test/auth/login',
+      data: {
+        email: 'brayanantonio816@gmail.com',
+        password: 'brayancito16'
+      }
+    }).then(response => llamar(response.data.access_token))
+
+    const llamar = (token) => {
+      axios.get('http://api_airbnb.test/user', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }).then(response => console.log(response.data))
+    }
+  }
+
+}
+
+</script>
+
+<style>
+</style>
---
+++ src/components/VueApp.vue
@@ -0,0 +1,45 @@
+<template >
+  <div id="fondo">
+    <div id="contenido">
+      <div >
+          <h1>Consumiendo API de CodeIgniter-Airbnb</h1>
+      </div>
+    </div>
+  </div>
+
+</template>
+  
+<script>
+export default {
+  setup() {
+
+  }
+}
+</script>
+
+<style>
+#contenido {
+  justify-content: center;
+  width: 500px;
+  height: 500px;
+  position: absolute;
+  top: 35%;
+  left: 60%;
+}
+
+#fondo {
+  width: 50%;
+  height: 830px;
+  background-image: url(https://a.storyblok.com/f/87002/1280x720/c23d81e2d2/escuela-vue-curso-vue-router.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+
+h1 {
+  color: white;
+}
+#color {
+  color: white;
+}
+</style>
